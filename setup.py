8a557bd27fe98ad73c215c0ca375a08198e64562
Andy Yang <andy.yang@twsit.com>
2023-07-26 15:06:38 +0800

    README.md update

--- setup.py
+++ setup.py
@@ -1,38 +1,30 @@
 # coding: utf-8
 import pathlib
 from setuptools import setup, find_packages
-
-def read_requirements(file):
-    with open(file) as f:
-        return f.read().splitlines()
-    
-def read_file(file):
-    with open(file) as f:
-        return f.read()
-
-HOME = pathlib.Path(__file__).parent.resolve()
-
+ 
+HERE = pathlib.Path(__file__).parent.resolve()
+ 
 PACKAGE_NAME = "sitTools"
 AUTHOR = "Andy Yang"
 AUTHOR_EMAIL = "andy.yang@twsit.com"
-URL = "https://github.com/sit-sys/cTools"
-DOWNLOAD_URL = "https://pypi.org/project/cTools/"
-
+URL = "https://github.com/sit-sys/sitTools"
+DOWNLOAD_URL = "https://pypi.org/project/sitTools/"
+ 
 LICENSE = "MIT"
-VERSION = read_file("VERSION")
-DESCRIPTION = "Useful tools for S.I.T. project use"
-LONG_DESCRIPTION = (HOME / "docs" / "README.md").read_text(encoding="utf8")
+VERSION = "0.0.1"
+DESCRIPTION = "Useful function tools for SIT project use."
+LONG_DESCRIPTION = (HERE / "docs" / "README.md").read_text(encoding="utf8")
 LONG_DESC_TYPE = "text/markdown"
-
-requirements = (HOME / "requirements.txt").read_text(encoding="utf8")
+ 
+requirements = (HERE / "requirements.txt").read_text(encoding="utf8")
 INSTALL_REQUIRES = [s.strip() for s in requirements.split("\n")]
-
-dev_requirements = (HOME / "dev_requirements.txt").read_text(encoding="utf8")
+ 
+dev_requirements = (HERE / "dev_requirements.txt").read_text(encoding="utf8")
 EXTRAS_REQUIRE = {"dev": [s.strip() for s in dev_requirements.split("\n")]}
-
+ 
 CLASSIFIERS = [f"Programming Language :: Python :: 3.{str(v)}" for v in range(7, 12)]
 PYTHON_REQUIRES = ">=3.8"
-
+ 
 setup(
     name=PACKAGE_NAME,
     version=VERSION,
